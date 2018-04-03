d9056011e41a9da759c508037534f1e5f16092b3
ikostan <igorkostan@gmail.com>
2018-04-02 17:16:01 -0700

    MagicalItem => properties implementation

--- GameCore/MagicalItem.cs
+++ GameCore/MagicalItem.cs
@@ -8,5 +8,8 @@ namespace GameCore
 {
     public class MagicalItem
     {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public int Power { get; set; }
     }
 }
